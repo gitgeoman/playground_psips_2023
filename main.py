1ec436d94336e511d292a0907824cbfd6dc8a429
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2023-11-06 12:56:27 +0100

    chore: add create functionality

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 from dane import users_list
 
+
+
+def add_user_to(users_list:list) -> None:
+    """
+    add object to list
+    :param users_list: list - user list
+    :return: None
+    """
+    name = input('podaj imie ?')
+    posts = input('podaj liczbe postow ?')
+    users_list.append({'name': name, 'posts': posts})
+
+add_user_to(users_list)
+add_user_to(users_list)
+add_user_to(users_list)
+
+
+
+
+
+
+# print(users_list)
 for user in users_list:
     print(f'Twój znajomy {user["name"]} dodał {user["posts"]}')
