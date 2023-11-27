e9d54066eca0f540be28637e2cfd94bb6f9676b7
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2023-11-27 13:14:50 +0100

    chore: get_coordinates form wiki page

--- notatki.py
+++ notatki.py
@@ -1,4 +1,26 @@
-from dane import users_list
-from utils.my_funtions import update_user
+#### BEAUTIFULSOUP4
 
-update_user(users_list)
+from bs4 import BeautifulSoup
+import requests
+import re
+
+# -*- coding: utf-8 -*-
+
+nazwy_miejscowości = ['Opoczno', 'Lublin','Gdańsk']
+def get_coordinates_of(city:str)->list[float,float]:
+
+    # pobranie strony internetowe
+    adres_URL = f'https://pl.wikipedia.org/wiki/{city}'
+    response = requests.get(url=adres_URL)
+    response_html = BeautifulSoup(response.text, 'html.parser')
+
+    # pobranie współrzędnych z treści strony internetowej
+    response_html_latitude = response_html.select('.latitude')[1].text  # . ponieważ class
+    response_html_latitude = float(response_html_latitude.replace(',', '.'))
+    response_html_longitude = response_html.select('.longitude')[1].text  # . ponieważ class
+    response_html_longitude = float(response_html_longitude.replace(',', '.'))
+
+    return [response_html_latitude, response_html_longitude]
+
+for item in nazwy_miejscowości:
+    print(get_coordinates_of(item))
