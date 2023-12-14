307c9612a5e648957e43ff6edcbd96fdf4ee338d
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2023-12-14 08:41:11 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+# settings.py
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
